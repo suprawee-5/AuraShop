85a1d99b19acea332df7180c4ce85e04defd09cb
suprawee-5 <67319010025@skntc.ac.th>
2025-03-19 14:15:55 +0700

    update

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,5 +1,5 @@
 // rnf
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { FlatList, RefreshControl, Image } from "react-native"
 import { View, Text } from "@/components/Themed"
 import { SafeAreaView } from "react-native-safe-area-context"
@@ -9,149 +9,180 @@ import { images } from "@/constants"
 import SearchInput from "@/components/SearchInput"
 
 export default function Home() {
-  
-  const products = [
-    {
-      id: 1,
-      name: "เรียนรู้การสร้างเว็บแอพพลิเคชั่นด้วย Python Django 5 และ Next.JS 15 ร่วมกับ Supabase",
-      price: 535,
-      imgurl:
-        "https://www.itgenius.co.th/assets/frondend/images/picturecourse/workshop_django_nextjs.jpg",
-      postDate: "10 Mar 2024 20:30",
-      description:
-        "เรียนรู้การสร้างเว็บแอพพลิเคชั่นด้วย Python Django 5 และ Next.JS 15 ร่วมกับ Supabase เพื่อสร้างเว็บแอพพลิเคชั่นที่มีประสิทธิภาพและมีความปลอดภัย รวมถึงการจัดการข้อมูลด้วย Supabase เพื่อสร้างเว็บแอพพลิเคชั่นที่มีประสิทธิภาพและมีความปลอดภัย",
-    },
-    {
-      id: 2,
-      name: "เรียนรู้การสร้างแอพพลิเคชั่นด้วย React Native และ Expo",
-      price: 299,
-      imgurl:
-        "https://www.itgenius.co.th/assets/frondend/images/picturecourse/Basic_React_Native_Expo_Pic_Course.jpg",
-      postDate: "12 Mar 2024 15:00",
-      description:
-        "เรียนรู้การสร้างแอพพลิเคชั่นด้วย React Native และ Expo เพื่อสร้างแอพพลิเคชั่นที่มีประสิทธิภาพและมีความปลอดภัย รวมถึงการจัดการข้อมูลด้วย Supabase เพื่อสร้างแอพพลิเคชั่นที่มีประสิทธิภาพและมีความปลอดภัย",
-    },
-    {
-      id: 3,
-      name: "เรียนรู้การสร้างเว็บแอพพลิเคชั่นด้วย Vue และ Nuxt 3 ร่วมกับ Prisma และ Supabase",
-      price: 399,
-      imgurl:
-        "https://www.itgenius.co.th/assets/frondend/images/picturecourse/Basic_Vue_Nuxt_Pic_Course.jpg",
-      postDate: "14 Mar 2024 10:00",
-      description:
-        "เรียนรู้การสร้างเว็บแอพพลิเคชั่นด้วย Vue และ Nuxt 3 ร่วมกับ Prisma และ Supabase เพื่อสร้างเว็บแอพพลิเคชั่นที่มีประสิทธิภาพและมีความปลอดภัย รวมถึงการจัดการข้อมูลด้วย Supabase เพื่อสร้างเว็บแอพพลิเคชั่นที่มีประสิทธิภาพและมีความปลอดภัย",
-    },
-    {
-      id: 4,
-      name: "เรียนรู้การสร้างเว็บแอพพลิเคชั่นด้วย Kubernetes (K8s) สำหรับผู้พัฒนาเว็บ",
-      price: 599,
-      imgurl:
-        "https://www.itgenius.co.th/assets/frondend/images/picturecourse/k8s-for-dev-cover.jpg",
-      postDate: "16 Mar 2024 11:00",
-      description:
-        "เรียนรู้การสร้างเว็บแอพพลิเคชั่นด้วย Kubernetes (K8s) เพื่อสร้างเว็บแอพพลิเคชั่นที่มีประสิทธิภาพและมีความปลอดภัย รวมถึงการจัดการข้อมูลด้วย Supabase เพื่อสร้างเว็บแอพพลิเคชั่นที่มีประสิทธิภาพและมีความปลอดภัย",
-    },
-    {
-      id: 5,
-      name: "Internet of Things with MicroPython สำหรับผู้เริ่มต้น",
-      price: 499,
-      imgurl:
-        "https://www.itgenius.co.th/assets/frondend/images/picturecourse/IOT_MicroPython_Cover.jpg",
-      postDate: "18 Mar 2024 12:00",
-      description:
-        "Learn to build web applications using MicroPython. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    },
-    {
-      id: 6,
-      name: "เรียนรู้การสร้างเว็บแอพพลิเคชั่นด้วย Go และ React",
-      price: 329,
-      imgurl:
-        "https://www.itgenius.co.th/assets/frondend/images/picturecourse/Go_and_React_PicCourse.jpg",
-      postDate: "20 Mar 2024 13:00",
-      description:
-        "เรียนรู้การสร้างเว็บแอพพลิเคชั่นด้วย Go และ React เพื่อสร้างเว็บแอพพลิเคชั่นที่มีประสิทธิภาพและมีความปลอดภัย รวมถึงการจัดการข้อมูลด้วย Supabase เพื่อสร้างเว็บแอพพลิเคชั่นที่มีประสิทธิภาพและมีความปลอดภัย",
-    },
-    {
-      id: 7,
-      name: "เรียนรู้การสร้างเว็บแอพพลิเคชั่นด้วย Web API Spring Boot 3 Kotlin ร่วมกับ Nuxt 3 และ Docker",
-      price: 379,
-      imgurl:
-        "https://www.itgenius.co.th/assets/frondend/images/picturecourse/SpringBoot3_With_Nuxt3_PicCourse.jpg",
-      postDate: "22 Mar 2024 14:00",
-      description:
-        "เรียนรู้การสร้างเว็บแอพพลิเคชั่นด้วย Web API Spring Boot 3 Kotlin ร่วมกับ Nuxt 3 และ Docker เพื่อสร้างเว็บแอพพลิเคชั่นที่มีประสิทธิภาพและมีความปลอดภัย รวมถึงการจัดการข้อมูลด้วย Supabase เพื่อสร้างเว็บแอพพลิเคชั่นที่มีประสิทธิภาพและมีความปลอดภัย",
-    },
-    {
-      id: 8,
-      name: "เรียนรู้การสร้างเว็บแอพพลิเคชั่นด้วย Laravel 11 ร่วมกับ Vue 3",
-      price: 389,
-      imgurl:
-        "https://www.itgenius.co.th/assets/frondend/images/picturecourse/Laravel11_Vue3_Pic_Course.jpg",
-      postDate: "24 Mar 2024 15:00",
-      description:
-        "เรียนรู้การสร้างเว็บแอพพลิเคชั่นด้วย Laravel 11 ร่วมกับ Vue 3 เพื่อสร้างเว็บแอพพลิเคชั่นที่มีประสิทธิภาพและมีความปลอดภัย รวมถึงการจัดการข้อมูลด้วย Supabase เพื่อสร้างเว็บแอพพลิเคชั่นที่มีประสิทธิภาพและมีความปลอดภัย",
-    },
-    {
-      id: 9,
-      name: "เรียนรู้การสร้างเว็บแอพพลิเคชั่นด้วย Angular 17 ร่วมกับ .NET Core 8",
-      price: 989,
-      imgurl:
-        "https://www.itgenius.co.th/assets/frondend/images/picturecourse/Angular_with_NET8_PictureCourse.jpg",
-      postDate: "26 Mar 2024 16:00",
-      description:
-        "เรียนรู้การสร้างเว็บแอพพลิเคชั่นด้วย Angular 17 ร่วมกับ .NET Core 8 เพื่อสร้างเว็บแอพพลิเคชั่นที่มีประสิทธิภาพและมีความปลอดภัย รวมถึงการจัดการข้อมูลด้วย Supabase เพื่อสร้างเว็บแอพพลิเคชั่นที่มีประสิทธิภาพและมีความปลอดภัย",
-    },
-    {
-      id: 10,
-      name: "สร้างรายงานด้วย Power BI ร่วมกับ Power Query สำหรับผู้เริ่มต้น",
-      price: 890,
-      imgurl:
-        "https://www.itgenius.co.th/assets/frondend/images/picturecourse/PowerBI_With_Power_Query_Pic_Course.jpg",
-      postDate: "28 Mar 2024 17:00",
-      description:
-        "เรียนรู้การสร้างรายงานด้วย Power BI ร่วมกับ Power Query เพื่อสร้างรายงานที่มีประสิทธิภาพและมีความปลอดภัย รวมถึงการจัดการข้อมูลด้วย Supabase เพื่อสร้างรายงานที่มีประสิทธิภาพและมีความปลอดภัย",
-    },
-  ]
 
-  const horizontalItems = [
-    {
-      id: 1,
-      title: "สมาทโฟนสำหรับผู้เริ่มต้นอินเทอร์เน็ต",
-      image:
-        "https://www.pixalate.com/hubfs/Blog_Media/Blog%20covers/shutterstock_224743036.jpg",
-    },
-    {
-      id: 2,
-      title: "Sample IOT for Beginner 2",
-      image:
-        "https://iottechnews.com/wp-content/uploads/2023/03/IoT-Runs-on-Arm-Paid-Ads_300x600_ST2.png",
-    },
-    {
-      id: 3,
-      title: "Sample IOT for Beginner 3",
-      image:
-        "https://www.verytechnology.com/wp-content/uploads/2022/10/nfciot.jpg",
-    },
-    {
-      id: 4,
-      title: "Sample IOT for Beginner 4",
-      image:
-        "https://thumbs.dreamstime.com/b/nfc-perto-de-uma-comunica%C3%A7%C3%A3o-do-campo-30837330.jpg",
-    },
-    {
-      id: 5,
-      title: "Sample IOT for Beginner 5",
-      image:
-        "https://thumbs.dreamstime.com/b/terminal-de-paiement-avec-la-carte-de-cr%C3%A9dit-sur-le-bureau-dans-la-boutique-57656151.jpg",
-    },
-  ]
+  // Product Data
+  // const products = [
+  //   {
+  //     id: 1,
+  //     name: "เรียนรู้การสร้างเว็บแอพพลิเคชั่นด้วย Python Django 5 และ Next.JS 15 ร่วมกับ Supabase",
+  //     price: 535,
+  //     imgurl:
+  //       "https://www.itgenius.co.th/assets/frondend/images/picturecourse/workshop_django_nextjs.jpg",
+  //     postDate: "10 Mar 2024 20:30",
+  //     description:
+  //       "เรียนรู้การสร้างเว็บแอพพลิเคชั่นด้วย Python Django 5 และ Next.JS 15 ร่วมกับ Supabase เพื่อสร้างเว็บแอพพลิเคชั่นที่มีประสิทธิภาพและมีความปลอดภัย รวมถึงการจัดการข้อมูลด้วย Supabase เพื่อสร้างเว็บแอพพลิเคชั่นที่มีประสิทธิภาพและมีความปลอดภัย",
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "เรียนรู้การสร้างแอพพลิเคชั่นด้วย React Native และ Expo",
+  //     price: 299,
+  //     imgurl:
+  //       "https://www.itgenius.co.th/assets/frondend/images/picturecourse/Basic_React_Native_Expo_Pic_Course.jpg",
+  //     postDate: "12 Mar 2024 15:00",
+  //     description:
+  //       "เรียนรู้การสร้างแอพพลิเคชั่นด้วย React Native และ Expo เพื่อสร้างแอพพลิเคชั่นที่มีประสิทธิภาพและมีความปลอดภัย รวมถึงการจัดการข้อมูลด้วย Supabase เพื่อสร้างแอพพลิเคชั่นที่มีประสิทธิภาพและมีความปลอดภัย",
+  //   },
+  //   {
+  //     id: 3,
+  //     name: "เรียนรู้การสร้างเว็บแอพพลิเคชั่นด้วย Vue และ Nuxt 3 ร่วมกับ Prisma และ Supabase",
+  //     price: 399,
+  //     imgurl:
+  //       "https://www.itgenius.co.th/assets/frondend/images/picturecourse/Basic_Vue_Nuxt_Pic_Course.jpg",
+  //     postDate: "14 Mar 2024 10:00",
+  //     description:
+  //       "เรียนรู้การสร้างเว็บแอพพลิเคชั่นด้วย Vue และ Nuxt 3 ร่วมกับ Prisma และ Supabase เพื่อสร้างเว็บแอพพลิเคชั่นที่มีประสิทธิภาพและมีความปลอดภัย รวมถึงการจัดการข้อมูลด้วย Supabase เพื่อสร้างเว็บแอพพลิเคชั่นที่มีประสิทธิภาพและมีความปลอดภัย",
+  //   },
+  //   {
+  //     id: 4,
+  //     name: "เรียนรู้การสร้างเว็บแอพพลิเคชั่นด้วย Kubernetes (K8s) สำหรับผู้พัฒนาเว็บ",
+  //     price: 599,
+  //     imgurl:
+  //       "https://www.itgenius.co.th/assets/frondend/images/picturecourse/k8s-for-dev-cover.jpg",
+  //     postDate: "16 Mar 2024 11:00",
+  //     description:
+  //       "เรียนรู้การสร้างเว็บแอพพลิเคชั่นด้วย Kubernetes (K8s) เพื่อสร้างเว็บแอพพลิเคชั่นที่มีประสิทธิภาพและมีความปลอดภัย รวมถึงการจัดการข้อมูลด้วย Supabase เพื่อสร้างเว็บแอพพลิเคชั่นที่มีประสิทธิภาพและมีความปลอดภัย",
+  //   },
+  //   {
+  //     id: 5,
+  //     name: "Internet of Things with MicroPython สำหรับผู้เริ่มต้น",
+  //     price: 499,
+  //     imgurl:
+  //       "https://www.itgenius.co.th/assets/frondend/images/picturecourse/IOT_MicroPython_Cover.jpg",
+  //     postDate: "18 Mar 2024 12:00",
+  //     description:
+  //       "Learn to build web applications using MicroPython. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  //   },
+  //   {
+  //     id: 6,
+  //     name: "เรียนรู้การสร้างเว็บแอพพลิเคชั่นด้วย Go และ React",
+  //     price: 329,
+  //     imgurl:
+  //       "https://www.itgenius.co.th/assets/frondend/images/picturecourse/Go_and_React_PicCourse.jpg",
+  //     postDate: "20 Mar 2024 13:00",
+  //     description:
+  //       "เรียนรู้การสร้างเว็บแอพพลิเคชั่นด้วย Go และ React เพื่อสร้างเว็บแอพพลิเคชั่นที่มีประสิทธิภาพและมีความปลอดภัย รวมถึงการจัดการข้อมูลด้วย Supabase เพื่อสร้างเว็บแอพพลิเคชั่นที่มีประสิทธิภาพและมีความปลอดภัย",
+  //   },
+  //   {
+  //     id: 7,
+  //     name: "เรียนรู้การสร้างเว็บแอพพลิเคชั่นด้วย Web API Spring Boot 3 Kotlin ร่วมกับ Nuxt 3 และ Docker",
+  //     price: 379,
+  //     imgurl:
+  //       "https://www.itgenius.co.th/assets/frondend/images/picturecourse/SpringBoot3_With_Nuxt3_PicCourse.jpg",
+  //     postDate: "22 Mar 2024 14:00",
+  //     description:
+  //       "เรียนรู้การสร้างเว็บแอพพลิเคชั่นด้วย Web API Spring Boot 3 Kotlin ร่วมกับ Nuxt 3 และ Docker เพื่อสร้างเว็บแอพพลิเคชั่นที่มีประสิทธิภาพและมีความปลอดภัย รวมถึงการจัดการข้อมูลด้วย Supabase เพื่อสร้างเว็บแอพพลิเคชั่นที่มีประสิทธิภาพและมีความปลอดภัย",
+  //   },
+  //   {
+  //     id: 8,
+  //     name: "เรียนรู้การสร้างเว็บแอพพลิเคชั่นด้วย Laravel 11 ร่วมกับ Vue 3",
+  //     price: 389,
+  //     imgurl:
+  //       "https://www.itgenius.co.th/assets/frondend/images/picturecourse/Laravel11_Vue3_Pic_Course.jpg",
+  //     postDate: "24 Mar 2024 15:00",
+  //     description:
+  //       "เรียนรู้การสร้างเว็บแอพพลิเคชั่นด้วย Laravel 11 ร่วมกับ Vue 3 เพื่อสร้างเว็บแอพพลิเคชั่นที่มีประสิทธิภาพและมีความปลอดภัย รวมถึงการจัดการข้อมูลด้วย Supabase เพื่อสร้างเว็บแอพพลิเคชั่นที่มีประสิทธิภาพและมีความปลอดภัย",
+  //   },
+  //   {
+  //     id: 9,
+  //     name: "เรียนรู้การสร้างเว็บแอพพลิเคชั่นด้วย Angular 17 ร่วมกับ .NET Core 8",
+  //     price: 989,
+  //     imgurl:
+  //       "https://www.itgenius.co.th/assets/frondend/images/picturecourse/Angular_with_NET8_PictureCourse.jpg",
+  //     postDate: "26 Mar 2024 16:00",
+  //     description:
+  //       "เรียนรู้การสร้างเว็บแอพพลิเคชั่นด้วย Angular 17 ร่วมกับ .NET Core 8 เพื่อสร้างเว็บแอพพลิเคชั่นที่มีประสิทธิภาพและมีความปลอดภัย รวมถึงการจัดการข้อมูลด้วย Supabase เพื่อสร้างเว็บแอพพลิเคชั่นที่มีประสิทธิภาพและมีความปลอดภัย",
+  //   },
+  //   {
+  //     id: 10,
+  //     name: "สร้างรายงานด้วย Power BI ร่วมกับ Power Query สำหรับผู้เริ่มต้น",
+  //     price: 890,
+  //     imgurl:
+  //       "https://www.itgenius.co.th/assets/frondend/images/picturecourse/PowerBI_With_Power_Query_Pic_Course.jpg",
+  //     postDate: "28 Mar 2024 17:00",
+  //     description:
+  //       "เรียนรู้การสร้างรายงานด้วย Power BI ร่วมกับ Power Query เพื่อสร้างรายงานที่มีประสิทธิภาพและมีความปลอดภัย รวมถึงการจัดการข้อมูลด้วย Supabase เพื่อสร้างรายงานที่มีประสิทธิภาพและมีความปลอดภัย",
+  //   },
+  // ]
 
+  // const horizontalItems = [
+  //   {
+  //     id: 1,
+  //     title: "สมาทโฟนสำหรับผู้เริ่มต้นอินเทอร์เน็ต",
+  //     image:
+  //       "https://www.pixalate.com/hubfs/Blog_Media/Blog%20covers/shutterstock_224743036.jpg",
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "Sample IOT for Beginner 2",
+  //     image:
+  //       "https://iottechnews.com/wp-content/uploads/2023/03/IoT-Runs-on-Arm-Paid-Ads_300x600_ST2.png",
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "Sample IOT for Beginner 3",
+  //     image:
+  //       "https://www.verytechnology.com/wp-content/uploads/2022/10/nfciot.jpg",
+  //   },
+  //   {
+  //     id: 4,
+  //     title: "Sample IOT for Beginner 4",
+  //     image:
+  //       "https://thumbs.dreamstime.com/b/nfc-perto-de-uma-comunica%C3%A7%C3%A3o-do-campo-30837330.jpg",
+  //   },
+  //   {
+  //     id: 5,
+  //     title: "Sample IOT for Beginner 5",
+  //     image:
+  //       "https://thumbs.dreamstime.com/b/terminal-de-paiement-avec-la-carte-de-cr%C3%A9dit-sur-le-bureau-dans-la-boutique-57656151.jpg",
+  //   },
+  // ]
+
+  const [products, setProducts] = useState([])
   const [refreshing, setRefreshing] = useState(false)
 
+  const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL
+  const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY
+
+  if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error("Missing Supabase environment variables")
+  } 
+
+  // สร้างฟังก์ชันเพื่อดึงข้อมูลจาก Supabase
+  const fetchProducts = async () => {
+    try {
+      const response = await fetch(`${supabaseUrl}/rest/v1/products`, {
+        headers: {
+          apikey: supabaseAnonKey,
+          'Authorization': `Bearer ${supabaseAnonKey}`,
+        },
+      })
+      const data = await response.json()
+      setProducts(data)
+    } catch (error) {
+      console.error("Error fetching products:", error)
+    }
+  }
+
+  // ดึงข้อมูลเมื่อ Component ถูกโหลด
+  useEffect(() => {
+    fetchProducts()
+  }, [])
+
+  // ฟังก์ชันสำหรับ Refresh ข้อมูล
   const onRefresh = async () => {
     setRefreshing(true)
-   
-    console.log("Fetching Data...")
+    // Fetch Data from API
+    await fetchProducts()
     setRefreshing(false)
   }
 
@@ -160,7 +191,7 @@ export default function Home() {
   }
 
   const handleProductCardPress = (item: any) => {
-    console.log("Product Card Pressed:", item.name)
+    console.log("Product Card Pressed:", item.title)
   }
 
   return (
@@ -173,7 +204,7 @@ export default function Home() {
                 <Text className="font-pmedium text-md text-gray-100">
                   ยินดีต้อนรับ
                 </Text>
-                <Text className="text-2xl text-white">Aura Shop</Text>
+                <Text className="text-2xl text-white">สามิตร โกยม</Text>
               </View>
 
               <View className="mt-1.5">
@@ -194,11 +225,11 @@ export default function Home() {
             </View>
             <FlatList
               horizontal
-              data={horizontalItems}
-              keyExtractor={(item) => item.id.toString()}
+              data={products.slice(0, 5)}
+              keyExtractor={(item:any) => item.id.toString()}
               renderItem={({ item }) => (
                 <HorizontalCard
-                  image={item.image}
+                  image={'https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg'}
                   title={item.title}
                   onPress={() => handleHorizontalCardPress(item)}
                 />
@@ -209,18 +240,17 @@ export default function Home() {
           </View>
         )}
         data={products}
-        keyExtractor={(item) => item.id.toString()}
+        keyExtractor={(item: any) => item.id.toString()}
         renderItem={({ item }) => (
           <ProductCard
-            productname={item.name}
+            productname={item.title}
             productprice={item.price}
-            productimage={item.imgurl}
-            postDate={item.postDate}
+            productimage={'https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg'}
+            postDate={item.created_at}
             description={item.description}
             onPress={() => handleProductCardPress(item)}
           />
         )}
-
         ListEmptyComponent={() => (
           <Text className="text-center">ไม่พบสินค้า</Text>
         )}
